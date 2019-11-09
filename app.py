30cbb7c3d71430bc519c93442dc0bc7a90886f4d
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-11-09 16:48:15 +0000

    Linked database

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'shopping_helper'
+app.config["MONGO_URI"] = 'mongodb+srv://root:Pablo51@myfirstcluster-8ubao.mongodb.net/shopping_helper?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def index():
-    return 'Hello world... again'
+@app.route("/get_items")
+def get_items():
+    return render_template("items.html", items=mongo.db.inventory.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)

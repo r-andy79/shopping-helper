0f697c11dce3ba475dd0303ac37773c92fef586f
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-11-09 14:18:10 +0000

    Added env variables

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return 'Hello world... again'
 
-@app.route("/about")
-def about():
-  return render_template("about.html")
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
